a1cc7dece22221cbd1f821c033c07e554573ddbe
Jacob Stewart <2606873+unitoftime@users.noreply.github.com>
2022-05-19 10:14:09 -0400

    go fmt and gitignore

--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
-  "fmt"
-  "log"
-	"time"
 	"strings"
-  "io/ioutil"
+	"time"
 
-
-  "golang.org/x/net/context"
-  "golang.org/x/oauth2/google"
-  "google.golang.org/api/youtube/v3"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/youtube/v3"
 )
 
 func main() {
@@ -22,32 +21,34 @@ func main() {
 	livestreamId := os.Args[1]
 	log.Println(livestreamId)
 
-  ctx := context.Background()
+	ctx := context.Background()
 
-  b, err := ioutil.ReadFile("token.json")
-  if err != nil {
-    log.Fatalf("Unable to read client secret file: %v", err)
-  }
+	b, err := ioutil.ReadFile("token.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
 
-  // If modifying these scopes, delete your previously saved credentials
-  // at ~/.credentials/youtube-go-quickstart.json
-  config, err := google.ConfigFromJSON(b, youtube.YoutubeScope)
-  if err != nil {
-    log.Fatalf("Unable to parse client secret file to config: %v", err)
-  }
-  client := getClient(ctx, config)
-  service, err := youtube.New(client)
+	// If modifying these scopes, delete your previously saved credentials
+	// at ~/.credentials/youtube-go-quickstart.json
+	config, err := google.ConfigFromJSON(b, youtube.YoutubeScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(ctx, config)
+	service, err := youtube.New(client)
 
-  handleError(err, "Error creating YouTube client")
+	handleError(err, "Error creating YouTube client")
 
 	chatIds := fetchChatIds([]string{livestreamId}, service)
 
 	liveChatId, ok := chatIds[livestreamId]
-	if !ok { panic("Failed to get chat ID") }
+	if !ok {
+		panic("Failed to get chat ID")
+	}
 
 	fmt.Println("Live chat Id", liveChatId)
 	nooter := Noot{
-		service: service,
+		service:    service,
 		liveChatId: liveChatId,
 	}
 
@@ -58,7 +59,7 @@ func main() {
 }
 
 type Noot struct {
-	service *youtube.Service
+	service    *youtube.Service
 	liveChatId string
 }
 
@@ -111,29 +112,30 @@ func (n *Noot) Listen() {
 
 		commands := []Command{
 			Command{
-				Name: "!echo",
+				Name:    "!echo",
 				Handler: EchoCommander{},
 			},
 			Command{
-				Name: "!recursion",
+				Name:    "!recursion",
 				Handler: RecursionCommander{},
 			},
 		}
 
-
 		for _, item := range resp.Items {
 			for _, cmd := range commands {
 				prefix, postfix, found := strings.Cut(item.Snippet.DisplayMessage, cmd.Name)
-				if !found { continue }
+				if !found {
+					continue
+				}
 
 				message := Message{
 					Author: User{
-						Id: item.AuthorDetails.ChannelId,
+						Id:   item.AuthorDetails.ChannelId,
 						Name: item.AuthorDetails.DisplayName,
 					},
 					Parsed: ParsedMessage{
 						Command: cmd.Name,
-						Prefix: prefix,
+						Prefix:  prefix,
 						Postfix: postfix,
 					},
 				}
@@ -148,23 +150,23 @@ func (n *Noot) Listen() {
 }
 
 type User struct {
-	Id string // This is the channel id. I think this can't change?
+	Id   string // This is the channel id. I think this can't change?
 	Name string // This is the users current display name. I think this can change
 }
 
 type ParsedMessage struct {
 	Command string // This is the command that was detected
-	Prefix string  // This is string before the command
+	Prefix  string // This is string before the command
 	Postfix string // This is the string after the command
 }
 
 type Message struct {
-	Author User // This is the person who sent the Message
+	Author User          // This is the person who sent the Message
 	Parsed ParsedMessage // This is the parsed message
 }
 
 type Command struct {
-	Name string // The command string to search for
+	Name    string // The command string to search for
 	Handler Commander
 }
 
